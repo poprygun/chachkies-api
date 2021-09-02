3a5aaaf1948ed470718a42f30bd8a6e0ffb1be8a
Alex Shumilov <ashumilov@vmware.com>
2021-09-02 11:54:27 -0400

    Add import generated file ref.

--- src/main/kotlin/io/microsamples/accelerator/chachkies/api/ChachkiesApiServiceImpl.kt
+++ src/main/kotlin/io/microsamples/accelerator/chachkies/api/ChachkiesApiServiceImpl.kt
@@ -1,12 +1,13 @@
 package io.microsamples.accelerator.chachkies.api
 
 import io.microsamples.accelerator.chachkies.model.Chachkie
+import io.microsamples.accelerator.chachkies.api.ChachkiesChachkieService
 import org.jeasy.random.EasyRandom
 import org.springframework.stereotype.Service
 import kotlin.streams.toList
 
 @Service
-class ChachkiesChachkieServiceImpl : ChachkiesChachkieService {
+class ChachkiesApiServiceImpl : ChachkiesChachkieService {
     override fun getChachkies(): List<Chachkie> {
        return EasyRandom().objects(Chachkie::class.java, 13).toList()
     }
