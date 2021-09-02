e20238a0180442ef13babd4b37f3efad01153950
Alex Shumilov <ashumilov@vmware.com>
2021-09-02 15:09:38 -0400

    Add scan packages.

--- src/main/kotlin/io/microsamples/accelerator/chachkies/Application.kt
+++ src/main/kotlin/io/microsamples/accelerator/chachkies/Application.kt
@@ -3,7 +3,7 @@ package io.microsamples.accelerator.chachkies
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = ["io.microsamples.accelerator.chachkies", "io.microsamples.accelerator.chachkies.api"])
 class ChachkiesApiApplication
 
 fun main(args: Array<String>) {
